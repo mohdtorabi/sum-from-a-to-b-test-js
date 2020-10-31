a86f45c2872eb3d0a6b32a48fd92ef5499cc4ff3
mohdtorabi <mitq8.mt@gmail.com>
2020-10-31 05:43:51 +0000

    done deal

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN;
+  }
+  return toN + sum(fromN, (toN - 1));
 }
+
+
+
+console.log(sum(3,7));
 
 module.exports = sum;
